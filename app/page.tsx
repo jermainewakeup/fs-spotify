f28266c06784d67944ecf1aa7821df32aff4fb25
Jermainewakeup <jermainewakeup@gmail.com>
2025-09-10 13:17:50 -0700

    Test button v2

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
   return (
       <main className="flex min-h-screen flex-col items-center justify-center w-full">
         <h1 className="text-3xl font-bold">Hey, fs-spotify!</h1>
-        <button className="flex text-2xl font-light">BUTTON HEYO</button>
+        <button className="mt-6 rounded-md text-white bg-red-600 hover px-2 py-4">
+          dont press me
+        </button>
       </main>
   );
 }
