61f607e54aa98f1dc9551d88ba66d95e55bdedb0
Jermainewakeup <jermainewakeup@gmail.com>
2025-09-10 13:05:06 -0700

    Test button

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
       <main className="flex min-h-screen flex-col items-center justify-center w-full">
         <h1 className="text-3xl font-bold">Hey, fs-spotify!</h1>
+        <button className="flex">BUTTON HEYO</button>
       </main>
   );
 }
