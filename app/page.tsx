61e0624cf0a176db12ab59f9886b3e43289e14a2
Jermainewakeup <jermainewakeup@gmail.com>
2025-09-10 14:38:19 -0700

    testing out Dog API and fetching (roof!)

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
-import Image from "next/image";
-
-export default function Home() {
+export default async function Home() {
+  const data = await fetch("https://dog.ceo/api/breeds/image/random")
+  const posts = await data.json()
   return (
       <main className="flex min-h-screen flex-col items-center justify-center w-full">
         <h1 className="text-3xl font-bold">Hey, fs-spotify!</h1>
-        <button className="mt-6 rounded-md text-white bg-red-600 hover px-2 py-4">
+        <button onClick={() => alert("YOU PRESSED IT")}
+                className="mt-6 rounded-md text-white bg-red-600 hover px-2 py-4">
           dont press me
         </button>
+        <div className="mt-8">
+          <p className={"mb-2 text-lg"}>Rando doggo:</p>
+          <img
+            src={data.message}
+            alt={"random dog"}
+            className="rounded-md text-white bg-red-600 hover px-2 py-4" />
+        </div>
+
       </main>
   );
 }
