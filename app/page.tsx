17c85b4d88ff851a3a1799fe3e5c4308ccb996d4
Jermainewakeup <jermainewakeup@gmail.com>
2025-09-22 15:39:30 -0700

    Auth.js logic implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,11 @@
-import {dogImage} from "@/app/dogs";
-
 export default async function Home() {
-  return (
-      <main className="flex min-h-screen flex-col items-center justify-center w-full">
-        <h1 className="text-3xl font-bold">Hey, fs-spotify!</h1>
-
-        <div className="mt-8 flex flex-col items-center">
-          <p className={"mb-2 text-lg"}>Rando doggo:</p>
-          <img
-            src={await dogImage()}
-            alt={"random dog"}
-            className="rounded-md max-w-xs shadow-md"
-          />
-        </div>
-      </main>
-  );
+    return (
+        <main>
+            <h1 className={"flex flex-col mt-12 items-center h-screen font-bold shadow-2xl"}>
+                Your Top Artists
+            </h1>
+            <ul>
+            </ul>
+        </main>
+    );
 }
