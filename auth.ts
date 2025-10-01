ea44031d3066b13c5c86d7aac05abe9df35e99da
Jermainewakeup <jermainewakeup@gmail.com>
2025-10-01 12:30:54 -0700

    URL fixes

--- auth.ts
+++ auth.ts
@@ -3,6 +3,7 @@ import Spotify from "next-auth/providers/spotify"
 import {PrismaAdapter} from "@auth/prisma-adapter";
 import { prisma } from "@/prisma"
 export const { handlers, signIn, signOut, auth } = NextAuth({
+    secret: process.env.AUTH_SECRET,
     adapter: PrismaAdapter(prisma),
     session : {strategy: "jwt"},
     providers: [
